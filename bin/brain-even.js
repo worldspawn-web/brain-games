09d3cd880c8855726eef26bfb3bd15775819b729
worldspawn <worldspawn.web@gmail.com>
2023-03-22 19:36:21 +0300

    binaries linter fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable no-console, import/extensions */
 /* eslint linebreak-style: off */
-import brainEvenGame from "../src/games/brain-even.js";
+import brainEvenGame from '../src/games/brain-even.js';
 
 brainEvenGame();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable no-console, import/extensions */
 /* eslint linebreak-style: off */
-import userNameAsk from "../src/cli.js";
+import userNameAsk from '../src/cli.js';
 
 userNameAsk();
