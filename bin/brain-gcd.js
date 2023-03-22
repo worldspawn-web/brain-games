e082d35502278a3eb59d53f0415061b2d7446c29
worldspawn <worldspawn.web@gmail.com>
2023-03-22 13:14:43 +0300

    bugfix with 0 value being the correct answer

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable no-console, import/extensions */
 /* eslint linebreak-style: off */
-import brainGcdGame from "../src/games/brain-gcd.js";
+import brainGcdGame from '../src/games/brain-gcd.js';
 
 brainGcdGame();
