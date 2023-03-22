58cd8bcd74056edee71a7af9bfa2d18093db343b
worldspawn <worldspawn.web@gmail.com>
2023-03-22 16:23:27 +0300

    brain-progression added

---
+++ bin/brain-progression.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+/* eslint-disable no-console, import/extensions */
+/* eslint linebreak-style: off */
+
+import brainProgressionGame from '../src/games/brain-progression.js';
+
+brainProgressionGame();
---
+++ src/games/brain-progression.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-duplicates */
+/* eslint-disable no-console, import/extensions */
+/* eslint linebreak-style: off */
+
+import readlineSync from 'readline-sync';
+import { wrongMsg } from '../index.js';
+import { winLogic } from '../index.js';
+import { randomNumber } from '../index.js';
+import { userGreeting } from '../index.js';
+
+const brainProgressionGame = () => {
+  // greeting
+  const userName = userGreeting();
+  // tempvars
+  let firstRandNumber;
+  let progressionValue;
+  let calcAnswer;
+  let userAnswer;
+  for (let i = 0; i < 3; i += 1) {
+    // generates random first item of array
+    firstRandNumber = randomNumber(100);
+    // diff between [0] and [1]
+    progressionValue = randomNumber(20);
+    // makes an array empty for each iteration
+    const resultArray = [firstRandNumber];
+    // pushes 10 numbers to the brand new fresh array
+    for (let j = 0; j < 10; j += 1) {
+      firstRandNumber += progressionValue;
+      resultArray.push(firstRandNumber);
+    }
+    // tempvar to store the index of chosen item
+    const skipStorage = randomNumber(10);
+    // saves the answer from array index
+    calcAnswer = resultArray[skipStorage];
+    // replaces the answer with dots
+    resultArray[skipStorage] = '...';
+
+    console.log('What number is missing in the progression?');
+    console.log(resultArray);
+    userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === calcAnswer.toString()) {
+      console.log('Correct!');
+    } else {
+      wrongMsg(userAnswer, calcAnswer, userName);
+      break;
+    }
+    // game ends on win
+    winLogic(i, userName);
+  }
+};
+export default brainProgressionGame;
